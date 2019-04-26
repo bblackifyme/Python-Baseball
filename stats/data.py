0fe68e14d1cc02bee6886db2a7738e854ee7e1d2
administrator <administrator@administorsimac.lan>
2019-04-26 09:06:48 -0700

    Completed the module

--- stats/data.py
+++ stats/data.py
@@ -16,7 +16,7 @@ identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
 identifiers = identifiers.fillna(method='ffill')
 identifiers.columns = ['game_id', 'year']
 
-games = pd.concat([identifiers, games], axis=1, sort=False)
+games = pd.concat([games, identifiers], axis=1, sort=False)
 games = games.fillna(' ')
 
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
